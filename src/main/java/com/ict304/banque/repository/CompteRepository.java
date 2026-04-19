a20dc528885b278586b75f2af493330a1f0f1e91
NaolieGit <naolieema@gmail.com>
2026-04-19 17:19:30 +0100

    Nettoyage du projet et ajout des sources Spring Boot

--- src/main/java/com/ict304/banque/repository/CompteRepository.java
+++ src/main/java/com/ict304/banque/repository/CompteRepository.java
@@ -1,10 +1,13 @@
 package com.ict304.banque.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ict304.banque.model.Compte;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
 @Repository
 public interface CompteRepository extends JpaRepository<Compte, Long> {
+
 }
